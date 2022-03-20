735eeeb285ab5a1c3eeb289ec540d14a660530d3
amandaqiu1 <71526639+amandaqiu1@users.noreply.github.com>
2022-03-20 18:41:23 -0400

    profile and inspect pages

---
+++ front-end/src/Item.js
@@ -0,0 +1,21 @@
+import "./Item.css"
+
+const Item = props => {
+    return (
+    <article className="Item">
+    <h1 className="Name">{props.name}</h1>
+      <div className="Item-details">
+        <p className="Quantity">
+          <div id = "quantity1">Quantity</div>
+           <div id="quantity2">{props.quantity}</div>
+        </p>
+        <div id="notes">
+          <b>Notes</b>
+          <p>{props.details}</p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Item
