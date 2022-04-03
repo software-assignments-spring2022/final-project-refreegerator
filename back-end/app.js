61c1e963e0d9a69d33a4632930866c516ba5cfae
Amanda Qiu <aq438@nyu.edu>
2022-04-03 16:44:19 -0400

    post data for add item

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,35 @@
 const express = require("express") 
 const app = express()
+const bodyParser = require("body-parser")
 
+app.use(express.json()) // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+/*
+app.post('/Profile',(req, res) => {
+  const data = {
+    days: req.body.days,
+    suggest: req.body.suggest,
+    auto:req.body.auto
+  }
+  console.log(data)
+  res.json(data)
+})*/
+app.post('/Add', (req, res)=>{
+  const data = {
+    name: req.body.name,
+    quantity: req.body.quantity,
+    category: req.body.category,
+    info: req.body.info,
+    ex_date:req.body.ex_date,
+    enableAlerts: req.body.enableAlerts,
+    notif: req.body.notif
+  }
+  console.log(data)
+  res.json(data)
+})
 module.exports = app
+
