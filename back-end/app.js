433c5aa70c2af822b70d9281191dba733cb4c09d
Amanda Qiu <aq438@nyu.edu>
2022-04-04 00:29:58 -0400

    profile post

--- back-end/app.js
+++ back-end/app.js
@@ -1,35 +1,25 @@
-const express = require("express") 
-const app = express()
-const bodyParser = require("body-parser")
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
+const express = require('express') // CommonJS import style!
+const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
+const cors = require('cors') 
+
+const app = express() // instantiate an Express object
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' })) // log all incoming requests, except when in unit test mode.  morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(cors()) // allow cross-origin resource sharing
 
 app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
 
-/*
-app.post('/Profile',(req, res) => {
+
+app.post('/profile/save', async (req, res) => {
   const data = {
     days: req.body.days,
     suggest: req.body.suggest,
-    auto:req.body.auto
+    auto: req.body.auto
   }
-  console.log(data)
   res.json(data)
-})*/
-app.post('/Add', (req, res)=>{
-  const data = {
-    name: req.body.name,
-    quantity: req.body.quantity,
-    category: req.body.category,
-    info: req.body.info,
-    ex_date:req.body.ex_date,
-    enableAlerts: req.body.enableAlerts,
-    notif: req.body.notif
-  }
   console.log(data)
-  res.json(data)
 })
-module.exports = app
 
+module.exports = app
