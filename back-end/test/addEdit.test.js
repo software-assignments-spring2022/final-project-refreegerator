14c908779e9ce1b5954fe8531609e5e1836d41cc
Amanda Qiu <aq438@nyu.edu>
2022-04-08 00:05:07 -0400

    edit test

--- back-end/test/addEdit.test.js
+++ back-end/test/addEdit.test.js
@@ -17,3 +17,15 @@ describe("POST request to /add route", () => {
       })
   })
 })
+describe("POST request to /edit route", () => {
+  it("respond with an HTTP 200 status code and an object in the response body", done => {
+    chai
+      .request(server)
+      .post("/edit/save")
+      .end((err, res) => {
+        res.should.have.status(200) 
+        res.body.should.be.a("object") 
+        done() 
+      })
+  })
+})
