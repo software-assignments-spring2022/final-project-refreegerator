c2503665521700e99933c6ab9271c80ac9bc7a6a
ayt239 <alexanderyteng@gmail.com>
2022-04-08 14:09:54 -0400

    Preferences now persist on reload and reflect on the front end

---
+++ back-end/test/create.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("POST request to /Create route", () => {
+  it("respond with an HTTP 200 status code and an object in the response body", done => {
+    chai
+      .request(server)
+      .post("/create/save")
+      .end((err, res) => {
+        res.should.have.status(200) 
+        res.body.should.be.a('array') 
+        done() 
+      })
+  })
+})
